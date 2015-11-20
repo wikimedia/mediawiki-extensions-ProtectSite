fd38904d5179cc477d9ea106e4c59ff0c58db02a
Jack Phoenix <jack@countervandalism.net>
2015-11-20 14:40:14 +0200

    Version 0.5.0: extension registration support + various other fixes
    * Actually made this not fatal due to that stupid legacy require_once call in the body file
    * Globals -> RequestContext
    * wfMsg -> wfMessage
    * Changed MediaWikiBagOStuff initialization in ProtectSiteForm::__construct() to SqlBagOStuff initialization since the former was removed in MW 1.25, as per the core HISTORY file
    * Removed old PHP i18n file
    * Added missing i18n message "action-protectsite", which is shown as a part of a sentence in an error message when a user w/o the sufficient privileges tries to access Special:ProtectSite
    
    Change-Id: I8a0f3442fe0edaaa8cefcc919dc90a8fe2e3cd5f

--- ProtectSite.php
+++ ProtectSite.php
@@ -18,19 +18,11 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-/**
- * Protect against register_globals vulnerabilities.
- * This line must be present before any global variable is referenced.
- */
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( "This is not a valid entry point.\n" );
-}
-
 /* Extension Credits. Splarka wants me to be so UN:VAIN! Haet haet hat! */
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Protect Site',
-	'version' => '0.4.0',
+	'version' => '0.5.0',
 	'author' => array( '[http://en.uncyclopedia.co/wiki/User:Dawg Eric Johnston]', 'Chris Stafford', 'Jack Phoenix' ),
 	'descriptionmsg' => 'protectsite-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:ProtectSite',
@@ -54,83 +46,11 @@ $wgAvailableRights[] = 'protectsite';
 $wgGroupPermissions['bureaucrat']['protectsite'] = true;
 
 /* Add this special page to the special page listing array */
-$dir = dirname( __FILE__ ) . '/';
 $wgMessagesDirs['ProtectSite'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['ProtectSite'] = $dir . 'ProtectSite.i18n.php';
-$wgExtensionMessagesFiles['ProtectSiteAliases'] = $dir . 'ProtectSite.alias.php';
-$wgAutoloadClasses['ProtectSite'] = $dir . 'ProtectSite.body.php';
-$wgAutoloadClasses['ProtectSiteForm'] = $dir . 'ProtectSite.body.php';
+$wgExtensionMessagesFiles['ProtectSiteAliases'] = __DIR__ . '/ProtectSite.alias.php';
+$wgAutoloadClasses['ProtectSite'] = __DIR__ . '/ProtectSite.body.php';
+$wgAutoloadClasses['ProtectSiteForm'] = __DIR__ . '/ProtectSite.body.php';
 $wgSpecialPages['ProtectSite'] = 'ProtectSite';
 
 /* Register initialization function */
-$wgExtensionFunctions[] = 'wfSetupProtectsite';
-
-/**
- * Persistent data is unserialized from a record in the objectcache table
- * which is set in the Special page. It will change the permissions for
- * various functions for anonymous and registered users based on the data
- * in the array. The data expires after the set amount of time, just like
- * a block.
- */
-function wfSetupProtectsite() {
-	/* Globals */
-	global $wgGroupPermissions, $wgMemc, $wgProtectSiteExempt, $wgCommandLineMode;
-
-	// macbre: don't run code below when running in command line mode (memcache starts to act strange)
-	if ( !empty( $wgCommandLineMode ) ) {
-		return;
-	}
-
-	/* Initialize Object */
-	$persist_data = new SqlBagOStuff( array() );
-
-	/* Get data into the prot hash */
-	$prot = $wgMemc->get( wfMemcKey( 'protectsite' ) );
-	if( !$prot ) {
-		$prot = $persist_data->get( 'protectsite' );
-		if( !$prot ) {
-			$wgMemc->set( wfMemcKey( 'protectsite' ), 'disabled' );
-		}
-	}
-
-	/* Logic to disable the selected user rights */
-	if( is_array( $prot ) ) {
-		/* MW doesn't timeout correctly, this handles it */
-		if( time() >= $prot['until'] ) {
-			$persist_data->delete( 'protectsite' );
-		}
-
-		/* Protection-related code for MediaWiki 1.8+ */
-		$wgGroupPermissions['*']['createaccount'] = !( $prot['createaccount'] >= 1 );
-		$wgGroupPermissions['user']['createaccount'] = !( $prot['createaccount'] == 2 );
-
-		$wgGroupPermissions['*']['createpage'] = !( $prot['createpage'] >= 1 );
-		$wgGroupPermissions['*']['createtalk'] = !( $prot['createpage'] >= 1 );
-		$wgGroupPermissions['user']['createpage'] = !( $prot['createpage'] == 2 );
-		$wgGroupPermissions['user']['createtalk'] = !( $prot['createpage'] == 2 );
-
-		$wgGroupPermissions['*']['edit'] = !( $prot['edit'] >= 1 );
-		$wgGroupPermissions['user']['edit'] = !( $prot['edit'] == 2 );
-		$wgGroupPermissions['sysop']['edit'] = true;
-
-		$wgGroupPermissions['user']['move'] = !( $prot['move'] == 1 );
-		$wgGroupPermissions['user']['upload'] = !( $prot['upload'] == 1 );
-		$wgGroupPermissions['user']['reupload'] = !( $prot['upload'] == 1 );
-		$wgGroupPermissions['user']['reupload-shared'] = !( $prot['upload'] == 1 );
-
-		// are there any groups that should not get affected by ProtectSite's lockdown?
-		if( !empty( $wgProtectSiteExempt ) && is_array( $wgProtectSiteExempt ) ) {
-			// there are, so loop over them, and force these rights to be true
-			// will resolve any problems from inheriting rights from 'user' or 'sysop'
-			foreach( $wgProtectSiteExempt as $exemptGroup ) {
-				$wgGroupPermissions[$exemptGroup]['edit'] = 1;
-				$wgGroupPermissions[$exemptGroup]['createpage'] = 1;
-				$wgGroupPermissions[$exemptGroup]['createtalk'] = 1;
-				$wgGroupPermissions[$exemptGroup]['move'] = 1;
-				$wgGroupPermissions[$exemptGroup]['upload'] = 1;
-				$wgGroupPermissions[$exemptGroup]['reupload'] = 1;
-				$wgGroupPermissions[$exemptGroup]['reupload-shared'] = 1;
-			}
-		}
-	}
-}
+$wgExtensionFunctions[] = 'ProtectSite::setup';
