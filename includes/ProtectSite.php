f2ab26e09913b996757172b1083366a9e98ee42b
Fomafix <fomafix@googlemail.com>
2026-07-21 18:43:03 +0000

    Inject services
    Change-Id: I8842d71afc28423b682a6ebe9450631f40e6df31

--- includes/ProtectSite.php
+++ includes/ProtectSite.php
@@ -19,16 +19,17 @@
  */
 
 use MediaWiki\MediaWikiServices;
+use Wikimedia\ObjectCache\WANObjectCache;
 
 class ProtectSite extends FormSpecialPage {
 	/** @var SqlBagOStuff */
 	public $persist_data;
-	/** @var WANObjectCache */
-	public $wanCache;
 	/** @var array */
 	public $prot;
 
-	public function __construct() {
+	public function __construct(
+		private readonly WANObjectCache $wanCache,
+	) {
 		if ( version_compare( MW_VERSION, '1.46', '>=' ) ) {
 			parent::__construct( 'ProtectSite' );
 		} else {
@@ -63,7 +64,6 @@ class ProtectSite extends FormSpecialPage {
 		}
 
 		$this->persist_data = ObjectCache::getInstance( CACHE_DB );
-		$this->wanCache = MediaWikiServices::getInstance()->getMainWANObjectCache();
 
 		/* Get data into the value variable/array */
 		$this->prot = $this->wanCache->get( $this->wanCache->makeKey( 'protectsite' ) );
